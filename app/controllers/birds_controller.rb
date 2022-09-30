40dc7c7d5504fa7bdb2c0f8acf94358d9e4a3cc9
Joshua <mwalejoshua89@gmail.com>
2022-09-30 04:13:29 +0300

    finished delete action

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -33,6 +33,18 @@ class BirdsController < ApplicationController
     end
   end
 
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+#   bird.destroy
+# render json: {}
+
   def increment_likes
     bird = Bird.find_by(id: params[:id])
     if bird
